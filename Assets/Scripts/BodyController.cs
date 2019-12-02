a7e715b5ec1a798b4c1fbd56d96e4f75ef1c82dc
Adam <grzybojad@gmail.com>
2019-12-02 22:32:06 +0100

    Added project files

---
+++ Assets/Scripts/BodyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyController : MonoBehaviour
+{
+	public GameObject followTarget;
+
+	public float speed;
+	public float distance;
+
+	private Vector3 targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		targetPos = followTarget.transform.position;
+
+		transform.LookAt( targetPos );
+
+		if( (transform.position - targetPos).magnitude > distance )
+			transform.Translate( Vector3.forward * speed * Time.deltaTime );
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float turningSpeed;
+
+	//private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		//rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float horizontalInput = Input.GetAxis( "Horizontal" );
+		transform.Translate( Vector3.forward * speed * Time.deltaTime );
+		transform.Rotate( Vector3.up, horizontalInput * turningSpeed * Time.deltaTime );
+	}
+}
