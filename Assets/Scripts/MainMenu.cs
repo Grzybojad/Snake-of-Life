9ac4aca5fbb74b6486a5002afa007f4360d557fb
Adam <grzybojad@gmail.com>
2019-12-28 13:51:50 +0100

    Added a basic main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+	{
+		SceneManager.LoadScene( 1 );
+	}
+	public void QuitGame()
+	{
+		Debug.Log( "Exit " );
+		Application.Quit();
+	}
+}
