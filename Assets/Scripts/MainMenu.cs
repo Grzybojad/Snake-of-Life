c9aba7e57e11eb11050209780bb9e2457a12a680
Adam <grzybojad@gmail.com>
2019-12-30 17:07:57 +0100

    Implemented an options menu
    - You can now change the resolution, fullscreen mode, quality and volume in the options menu
    - The settings are saved in playerprefs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+	public GameObject firstOption;
+
+	private void OnEnable()
+	{
+		EventSystem.current.SetSelectedGameObject( null );
+		StartCoroutine( "SetFirstSelected" );
+	}
+
+	IEnumerator SetFirstSelected()
+	{
+		// Wait for 1 frame
+		yield return new WaitForEndOfFrame();
+
+		EventSystem.current.SetSelectedGameObject( firstOption );
+	}
+
+	public void PlayGame()
 	{
 		SceneManager.LoadScene( 1 );
 	}
+
 	public void QuitGame()
 	{
 		Debug.Log( "Exit " );
---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+	public Dropdown resolutionDropdown;
+	public Toggle fullscreenToggle;
+	public Dropdown qualityDropdown;
+	public Slider volumeSlider;
+
+	public AudioMixer masterMixer;
+
+	Resolution[] resolutions;
+
+	private void Start()
+	{
+		InitResolutions();
+		LoadOptionsValues();
+	}
+
+	private void OnEnable()
+	{
+		EventSystem.current.SetSelectedGameObject( resolutionDropdown.gameObject );
+	}
+
+	void InitResolutions()
+	{
+		resolutions = Screen.resolutions;
+		resolutionDropdown.ClearOptions();
+
+		List<string> resolutionStrings = new List<string>();
+
+		int currentResolutionIndex = 0;
+		for( int i = 0; i < resolutions.Length; ++i )
+		{
+			// Don't display resolutions that are too small
+			if( resolutions[ i ].width < 600 || resolutions[ i ].height < 500 ) continue;
+
+			string option = resolutions[ i ].width + " x " + resolutions[ i ].height;
+			resolutionStrings.Add( option );
+
+			if( resolutions[ i ].width == Screen.currentResolution.width &&
+				resolutions[ i ].height == Screen.currentResolution.height )
+			{
+				currentResolutionIndex = i;
+			}
+		}
+
+		resolutionDropdown.AddOptions( resolutionStrings );
+		resolutionDropdown.value = currentResolutionIndex;
+		resolutionDropdown.RefreshShownValue();
+	}
+
+	public void SaveOptionsValues()
+	{
+		PlayerPrefs.SetInt( "o_resolutionIndex", resolutionDropdown.value );
+		PlayerPrefs.SetInt( "o_fullscreen", fullscreenToggle.isOn ? 1 : 0 );
+		PlayerPrefs.SetInt( "o_qualityIndex", qualityDropdown.value );
+		PlayerPrefs.SetFloat( "o_master_volume", volumeSlider.value );
+	}
+
+	public void LoadOptionsValues()
+	{
+		resolutionDropdown.value = PlayerPrefs.GetInt( "o_resolutionIndex" );
+		fullscreenToggle.isOn = PlayerPrefs.GetInt( "o_fullscreen" ) == 1;
+		qualityDropdown.value = PlayerPrefs.GetInt( "o_qualityIndex" );
+		volumeSlider.value = PlayerPrefs.GetFloat( "o_master_volume" );
+	}
+
+
+	public void SetResolution( int resolutionIndex )
+	{
+		Resolution newRes = resolutions[ resolutionIndex ];
+		Screen.SetResolution( newRes.width, newRes.height, Screen.fullScreen );
+	}
+
+	public void SetQuality( int qualityIndex )
+	{
+		QualitySettings.SetQualityLevel( qualityIndex );
+	}
+
+	public void SetFullscreen( bool fullScreenMark )
+	{
+		Screen.fullScreen = fullScreenMark;
+	}
+
+	public void SetMasterVolume( float volume )
+	{
+		masterMixer.SetFloat( "volume", volume );
+	}
+}
