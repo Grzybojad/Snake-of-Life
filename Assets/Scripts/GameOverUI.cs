ee53cde768232f312b45abb3244f00b0e8b47d1e
Adam <grzybojad@gmail.com>
2019-12-30 17:28:18 +0100

    Fixed pause and game over menu navigation

--- Assets/Scripts/GameOverUI.cs
+++ Assets/Scripts/GameOverUI.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class GameOverUI : MonoBehaviour
 {
+	public GameObject firstOption;
+
+	private void OnEnable()
+	{
+		EventSystem.current.SetSelectedGameObject( firstOption );
+	}
+
 	public void PlayAgain()
 	{
 		SceneManager.LoadScene( 1 );
--- Assets/Scripts/PauseMenuUI.cs
+++ Assets/Scripts/PauseMenuUI.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class PauseMenuUI : MonoBehaviour
 {
+	public GameObject firstOption;
+
+	private void OnEnable()
+	{
+		EventSystem.current.SetSelectedGameObject( firstOption );
+	}
+
 	public void ResumeGame()
 	{
 		Time.timeScale = 1;
