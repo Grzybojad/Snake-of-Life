e61d0b8bb885c73c815d03856be9c74016fbc7e2
Adam <grzybojad@gmail.com>
2019-12-31 17:24:32 +0100

    Added constraints to the camera and extended background planes

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject focusObject;
+    public Vector3 cameraOffset;
+
+    // Camera boundry
+    public Vector2 minCameraValue;
+    public Vector2 maxCameraValue;
+
+    private Vector3 focusPos;
+
+    void Update()
+    {
+		focusPos = focusObject.transform.position;
+    
+		Vector3 pos = focusPos + cameraOffset;
+
+        // Clamp camera position
+        transform.position = new Vector3(
+            Mathf.Clamp( pos.x, minCameraValue.x, maxCameraValue.x ),
+            pos.y,
+            Mathf.Clamp( pos.z, minCameraValue.y, maxCameraValue.y )
+        );
+    }
+}
